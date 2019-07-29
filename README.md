9a91b9d2e437eeebc98675a3af41910dfecdeee0
wdpfm <16840517@qq.com>
2019-07-29 17:22:00 +0800

    文本、图片、容器、列表、水平列表、长列表、网格列表、包装控件、AppBar

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # 腾讯课堂亢少军老师的Flutter基础教程学习
-##文本、图片、容器、列表、水平列表
+## 文本、图片、容器、列表、水平列表、长列表、网格列表、包装控件、AppBar
+![1](https://github.com/wdpfm/flutter_ksj/blob/master/sc/cd.png)
+![2](https://github.com/wdpfm/flutter_ksj/blob/master/sc/hlvd.png)
+![3](https://github.com/wdpfm/flutter_ksj/blob/master/sc/id.png)
+![4](https://github.com/wdpfm/flutter_ksj/blob/master/sc/lvd.png)
+![5](https://github.com/wdpfm/flutter_ksj/blob/master/sc/td.png)
+![6](https://github.com/wdpfm/flutter_ksj/blob/master/sc/clb.png)
+![7](https://github.com/wdpfm/flutter_ksj/blob/master/sc/wglb.png)
+![8](https://github.com/wdpfm/flutter_ksj/blob/master/sc/row.png)
+![9](https://github.com/wdpfm/flutter_ksj/blob/master/sc/appbar.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,208 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(new HorizontalListViewDemo());
-class HorizontalListViewDemo extends StatelessWidget{
+void main() {
+  runApp(DidiSample());
+}
+
+class DidiSample extends StatefulWidget {
+  @override
+  _DidiSampleState createState() => _DidiSampleState();
+}
+
+class _DidiSampleState extends State<DidiSample> {
+  Choice _selectedChoice = choices[0];
+
+  void _select(Choice choice) {
+    setState(() {
+      _selectedChoice = choice;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final title='HorizontalListViewDemo';
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('滴滴出行'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(choices[0].icon),
+              onPressed: () {
+                _select(choices[0]);
+              },
+            ),
+            IconButton(
+              icon: Icon(choices[1].icon),
+              onPressed: () {
+                _select(choices[1]);
+              },
+            ),
+            PopupMenuButton<Choice>(
+              onSelected: _select,
+              itemBuilder: (BuildContext context) {
+                return choices
+                    .skip(2)
+                    .map<PopupMenuItem<Choice>>((Choice choice) {
+                  return PopupMenuItem<Choice>(
+                    value: choice,
+                    child: Text(choice.title),
+                  );
+                }).toList();
+              },
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ChoiceDidi(choice: _selectedChoice),
+        ),
+      ),
+    );
+  }
+}
+
+class Choice {
+  final String title;
+  final IconData icon;
+
+  const Choice({this.title, this.icon});
+}
+
+const List<Choice> choices = <Choice>[Choice(title: '自驾', icon: Icons.directions_car), Choice(title: '自行车', icon: Icons.directions_bike), Choice(title: '乘船', icon: Icons.directions_boat), Choice(title: '公交车', icon: Icons.directions_bus), Choice(title: '火车', icon: Icons.directions_railway), Choice(title: '步行', icon: Icons.directions_walk),];
+
+class ChoiceDidi extends StatelessWidget {
+  const ChoiceDidi({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              choice.icon,
+              size: 128.0,
+              color: textStyle.color,
+            ),
+            Text(
+              choice.title,
+              style: textStyle,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget packedRow = new Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new Icon(
+          Icons.star,
+          color: Colors.pinkAccent,
+        ),
+        new Icon(
+          Icons.star,
+          color: Colors.pinkAccent,
+        ),
+        new Icon(
+          Icons.star,
+          color: Colors.pinkAccent,
+        ),
+        new Icon(
+          Icons.star,
+          color: Colors.pinkAccent,
+        ),
+        new Icon(
+          Icons.star_border,
+          color: Colors.pinkAccent,
+        ),
+      ],
+    );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('包装控件'),
+      ),
+      body: packedRow,
+    );
+  }
+}
+
+class GridViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = '网格列表Demo';
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(40.0),
+          //边距
+          crossAxisSpacing: 70.0,
+          //间隙
+          crossAxisCount: 3,
+          children: <Widget>[
+            const Text('第1行第1列'),
+            const Text('第1行第2列'),
+            const Text('第1行第3列'),
+            const Text('第2行第1列'),
+            const Text('第2行第2列'),
+            const Text('第2行第3列'),
+            const Text('第3行第1列'),
+            const Text('第3行第2列'),
+            const Text('第3行第3列'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LongListViewDemo extends StatelessWidget {
+  final List<String> items;
+
+  LongListViewDemo({Key key, @required this.items}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final title = '长列表Demo';
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text(title),
+          ),
+          body: new ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return new ListTile(
+                leading: Icon(Icons.phone),
+                title: new Text('${items[index]}'),
+              );
+            },
+          )),
+    );
+  }
+}
+
+class HorizontalListViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'HorizontalListViewDemo';
     return new MaterialApp(
       title: title,
       home: new Scaffold(
@@ -22,8 +220,9 @@ class HorizontalListViewDemo extends StatelessWidget{
                 color: Colors.lightBlue,
                 child: new Text('blue',
                     style: new TextStyle(
-                  fontSize: 48.0,
-                ),textAlign: TextAlign.center),
+                      fontSize: 48.0,
+                    ),
+                    textAlign: TextAlign.center),
               ),
               new Container(
                 width: 160.0,
@@ -32,10 +231,11 @@ class HorizontalListViewDemo extends StatelessWidget{
                   children: <Widget>[
                     new Text(
                       'haha',
-                      style:new TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 36.0,
-                    ),),
+                      style: new TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 36.0,
+                      ),
+                    ),
                     new Text('lala'),
                     new Icon(Icons.camera_alt)
                   ],
@@ -64,10 +264,11 @@ class HorizontalListViewDemo extends StatelessWidget{
     );
   }
 }
-class ListViewDemo extends StatelessWidget{
+
+class ListViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title='ListViewDemo';
+    final title = 'ListViewDemo';
     return new MaterialApp(
       title: title,
       home: new Scaffold(
@@ -94,7 +295,8 @@ class ListViewDemo extends StatelessWidget{
     );
   }
 }
-class TextDemo extends StatelessWidget{
+
+class TextDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -106,7 +308,7 @@ class TextDemo extends StatelessWidget{
           new Text(
             '红色+黑色删除线+25号',
             style: new TextStyle(
-              color:const Color(0xffff0000),
+              color: const Color(0xffff0000),
               decoration: TextDecoration.lineThrough,
               decorationColor: const Color(0xff000000),
               fontSize: 25.0,
@@ -115,7 +317,7 @@ class TextDemo extends StatelessWidget{
           new Text(
             '橙色+下划线+24号',
             style: new TextStyle(
-              color:const Color(0xffff9900),
+              color: const Color(0xffff9900),
               decoration: TextDecoration.underline,
               fontSize: 24.0,
             ),
@@ -126,8 +328,7 @@ class TextDemo extends StatelessWidget{
                 decoration: TextDecoration.overline,
                 decorationStyle: TextDecorationStyle.dashed,
                 fontSize: 23.0,
-                fontStyle: FontStyle.italic
-            ),
+                fontStyle: FontStyle.italic),
           ),
           new Text(
             '22号+加粗',
@@ -142,12 +343,12 @@ class TextDemo extends StatelessWidget{
     );
   }
 }
-class ImageDemo extends StatelessWidget{
+
+class ImageDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Center(
-      child:
-      new Image.network(
+      child: new Image.network(
         'https://avatars0.githubusercontent.com/u/53364077?s=180&v=4',
         scale: 2.0,
         //fit: BoxFit.fill,
@@ -155,7 +356,8 @@ class ImageDemo extends StatelessWidget{
     );
   }
 }
-class ContainerDemo extends StatelessWidget{
+
+class ContainerDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Center(
@@ -164,14 +366,10 @@ class ContainerDemo extends StatelessWidget{
         height: 200.0,
         decoration: new BoxDecoration(
             color: Colors.amberAccent,
-            border: new Border.all(
-                color: const Color(0xff5d9eeb),
-                width: 8.0
-            ),
+            border: new Border.all(color: const Color(0xff5d9eeb), width: 8.0),
             borderRadius: const BorderRadius.all(
               const Radius.circular(18.0),
-            )
-        ),
+            )),
         child: new Text(
           'hello',
           textAlign: TextAlign.center,
@@ -187,6 +385,11 @@ class ContainerDemo extends StatelessWidget{
 //     //home: new ContainerDemo(),
 //     //home: new ImageDemo(),
 //     //home: new TextDemo(),
+//     //home: new LayoutDemo(),
 //  )
 //  );
 //}
+//void main()=>runApp(new GridViewDemo());
+//void main()=>runApp(new LongListViewDemo(
+//  items:new List<String>.generate(100, (i)=>"Item $i"),
+//));
